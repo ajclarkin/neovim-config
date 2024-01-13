01faf0694cbda1296a9c0d5dd8d1e6d391dc7787
ajclarkin <mail@ajclarkin.co.uk>
2024-01-13 23:00:18 +0000

    Lua LSP working, added some keybindings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "pyright"}
+                ensure_installed = {"lua_ls" }
             })
         end
     },
@@ -19,7 +19,10 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require('lspconfig')
-            lspconfig.lua_la.setup({})
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
