724a043ed22deff02225ee35518dfa67a29c9f81
ajclarkin <mail@ajclarkin.co.uk>
2024-01-23 17:35:41 +0000

    Auto-complete and snippets working

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -26,7 +26,9 @@ return {
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
+                    -- Lua
                     null_ls.builtins.formatting.stylua,
+                    -- Python
                     null_ls.builtins.formatting.ruff,
                     null_ls.builtins.formatting.black,
                     null_ls.builtins.formatting.isort,
