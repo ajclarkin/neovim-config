feef0243c071d078e07b6edbb2d4308771da57c3
ajclarkin <mail@ajclarkin.co.uk>
2024-03-06 14:52:58 +0000

    Added row and yank highlighting

--- lua/options.lua
+++ lua/options.lua
@@ -17,7 +17,7 @@ vim.opt.wrap = false
 
 -- Indent settings. Shiftwidth is for when we use indent operator >>
 vim.opt.expandtab = true
-vim.opt.tabstop = 4 
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 
@@ -39,3 +39,19 @@ vim.opt.inccommand = "split"
 vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
+
+-- Show the line that the curson is on
+vim.opt.cursorline = true
+
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
