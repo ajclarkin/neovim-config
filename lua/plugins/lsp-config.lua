7c039202df6b1313af88153d9e7dfe0b66657d65
ajclarkin <mail@ajclarkin.co.uk>
2024-06-24 14:37:59 +0000

    Ruff edits

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -53,12 +53,12 @@ return {
         },
       })
 
---      local on_attach = function(client, bufnr)
---        if client.name == "ruff_lsp" then
---          -- Disable hover in favor of Pyright
---          client.server_capabilities.hoverProvider = false
---        end
---      end
+      local on_attach = function(client, bufnr)
+        if client.name == "ruff_lsp" then
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end
 
       lspconfig.ruff.setup({
         capabilities = capabilities,
